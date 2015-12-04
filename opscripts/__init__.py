8a2e1c04920aa5115060ac2155e3fad573d127e1
Tim Zehta <tim@clockwork.net>
2015-12-04 10:13:07 -0600

    Renamed to OpScripts and fixed issues with setup
    - renamed to OpScripts from python-opscripts
    - added maintainer and maintaioner_mail
    - added config directory handling
    - added download_url
    - added MANIFEST.in
    - updated to use find_packages()

--- opscripts/__init__.py
+++ opscripts/__init__.py
@@ -2,8 +2,12 @@
 """Python libraries to assist with writing Linux Ops scripts.
 """
 
-__version__ = "0.9.0"
+__version__ = "0.9.4"
 
-__url__ = "https://github.com/ClockworkNet/python-opscripts"
+__maintainer__ = "Timid Robot Zehta"
+
+__maintainer_email__ = "tim@clockwork.com"
+
+__url__ = "https://github.com/ClockworkNet/OpScripts"
 
 __license__ = "MIT"
--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
 # vim: set fileencoding=utf-8 :
-"""python-optscrpits setup
+"""python-opscripts setup
 """
 # Standard library
+import distutils.sysconfig
+import os.path
 import re
+import site
+import sys
 # Third-party
-from setuptools import setup
+from setuptools import find_packages, setup
 
 re_info = re.compile("""
-        ^" " "(?P<description>.+)                   # description docstring
+        # Description docstring
+        ^" " "(?P<description>.+)
         ^" " ".*
-        __version__\s*=\s*"(?P<version>[^"]+)".*    # version variable
-        __url__\s*=\s*"(?P<url>[^"]+)".*            # url variable
-        __license__\s*=\s*"(?P<license>[^"]+)".*    # license variable
+        # Version variable
+        __version__\s*=\s*"(?P<version>[^"]+)".*
+        # Maintainer variable
+        __maintainer__\s*=\s*"(?P<maintainer>[^"]+)".*
+        # Maintainer_email variable
+        __maintainer_email__\s*=\s*"(?P<maintainer_email>[^"]+)".*
+        # URL variable
+        __url__\s*=\s*"(?P<url>[^"]+)".*
+        # License variable
+        __license__\s*=\s*"(?P<license>[^"]+)".*
         """, re.DOTALL | re.MULTILINE | re.VERBOSE)
 with open("opscripts/__init__.py", "rb") as f:
     results = re_info.search(f.read().decode("utf-8"))
-    description, version, url, license = results.groups()
+    metadata = results.groupdict()
 with open("README.rst", "rb") as f:
     long_description = f.read().decode("utf-8")
 install_requires = ["ConfigArgParse"]
@@ -25,12 +37,32 @@ classifiers = ["License :: OSI Approved :: MIT License",
                "Programming Language :: Python :: 2.6",
                "Programming Language :: Python :: 2.7",
                "Programming Language :: Python :: Implementation :: CPython"]
+packages = find_packages(".")
+print "--"
+from pprint import pprint
+pprint(packages)
+print "--"
+# Install config file appropriately
+config_file_path = "etc/opscripts"
+if hasattr(sys, "real_prefix"):
+    config_file_path = os.path.join(sys.prefix, config_file_path)
+elif "--user" in sys.argv:
+    config_file_path = os.path.join(site.USER_BASE, config_file_path)
+else:
+    config_file_path = os.path.join(distutils.sysconfig.get_python_lib(),
+                                 config_file_path)
 
-setup(name="python-opscripts",
-      version=version,
-      license=license,
-      description=description,
+setup(name="OpScripts",
+      version=metadata["version"],
+      maintainer=metadata["maintainer"],
+      maintainer_email=metadata["maintainer_email"],
+      license=metadata["license"],
+      description=metadata["description"],
       long_description=long_description,
-      url=url,
+      url=metadata["url"],
+      packages=packages,
+      data_files=[(config_file_path, [".placeholder"])],
       keywords="CLI, DevOps, Ops, sysadmin, Systems administration",
-      classifiers=classifiers)
+      classifiers=classifiers,
+      download_url="https://github.com/ClockworkNet/OpScripts/releases",
+      zip_safe=True)
