1fc72a3c4205f021867343534057ecd008f5c311
Tim Zehta <tim@clockwork.net>
2016-08-18 11:41:02 -0500

    added simple find_outdated_imports.sh utility

---
+++ find_outdated_imports.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo 'Current Versions
+================
+config        v5
+logging       v2
+notify.email  v3
+utils         v7
+yaml          v1
+'
+config='config import v[1-4]'
+logging='logging import v1'
+notify_email='notify[.]email import v[1-2]'
+utils='utils import v[1-6]'
+regex="^from opscripts[.](${config}|${logging}|${notify_email}|${utils})"
+ack --python "${regex}"
