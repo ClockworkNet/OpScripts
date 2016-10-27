75af64adad3963d83fce931f7c7cf8b3a9dbb60f
Tim Zehta <tim@clockwork.net>
2016-10-27 14:49:12 -0500

    removed cruft: older module versions

--- opscripts/config/v4.py
+++ opscripts/config/v4.py
@@ -12,7 +12,7 @@ import sys
 import configargparse
 
 # Local/library specific
-from opscripts.notify.email import v1 as ops_notify_email
+from opscripts.notify.email import v2 as ops_notify_email
 
 
 def OpsConfigArgParse(**kwargs):
